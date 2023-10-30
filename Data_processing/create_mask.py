c7fe30c2d3a9f79d8fbcb10b61bf656353ba526c
Simon Schwarz <schwarz_simon@outlook.de>
2023-10-30 18:41:02 +0100

    Resize masks

--- Data_processing/create_mask.py
+++ Data_processing/create_mask.py
@@ -4,50 +4,40 @@ import cv2
 import numpy as np
 from PIL import Image
 
-directory = "/Users/ludvigbennbom/Desktop/train/"
+directory = "F:/Dataset/small_dataset/"
 
 category_colors = {
-    1: (255, 0, 0),    # Red
-    2: (0, 255, 0),    # Green
-    3: (0, 0, 255),    # Blue
+    1: (255, 0, 0),  # Red
+    2: (0, 255, 0),  # Green
+    3: (0, 0, 255),  # Blue
     4: (255, 255, 0),  # Yellow
     5: (255, 0, 255),  # Magenta
     6: (0, 128, 0),  # Cyan
-    7: (128, 0, 0),    # Maroon
-    8: (0, 255, 255),    # Green (Dark)
-    9: (0, 0, 128),    # Navy
+    7: (128, 0, 0),  # Maroon
+    8: (0, 255, 255),  # Green (Dark)
+    9: (0, 0, 128),  # Navy
     10: (128, 128, 0),  # Olive
     11: (128, 0, 128),  # Purple
     12: (0, 128, 128),  # Teal
-    13: (128, 128, 128) # Gray
+    13: (128, 128, 128)  # Gray
 }
+
+
 def get_file_paths():
-    file_paths = os.listdir("/Users/ludvigbennbom/Desktop/small_dataset/json")
+    file_paths = os.listdir(directory + "json")
     return file_paths
+
 
 def create_mask():
     json_file_paths = get_file_paths()
 
     for file_name in json_file_paths:
-        data = json.load(open("/Users/ludvigbennbom/Desktop/small_dataset/json/" + file_name))
+        data = json.load(open(directory + "json/" + file_name))
         img_path = file_name.split(".")[0] + ".jpg"
-        img = cv2.imread("/Users/ludvigbennbom/Desktop/train/image/" + img_path)
-        mask = np.zeros_like(img, dtype=np.uint8)
-        for key in data:
-            if key.startswith("item"):
-                segmentation_list = data[key]["segmentation"]
-                category_id = data[key]["category_id"]
-                color = category_colors.get(category_id, (0, 0, 0))
-                ##points = np.array(segmentation_list)
-                for polygon_coords in segmentation_list:
-                    polygon_coords = np.array(polygon_coords, dtype=np.float32).reshape((-1, 2))
-                    polygon_coords = polygon_coords.astype(np.int32)  
-
-                    cv2.fillPoly(mask, [polygon_coords], color)  
-
-                # Displaying the image
-        mask_image = Image.fromarray(mask)
-        mask_image.save('/Users/ludvigbennbom/Desktop/small_dataset/masks/' + file_name.split(".")[0] + '_mask.png')  # Save with a unique name
+        img = cv2.imread(directory + "/images/" + img_path)
+        img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(directory + '/resized_images/' + file_name.split(".")[
+            0] + '_mask.png', img)  # Save with a unique name
 
 
 create_mask()
