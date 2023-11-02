c76c5eeba3bbb86948e4e047dc860316f5ea15e3
Simon <schwarz_simon@outlook.de>
2023-11-02 13:55:57 +0100

    Decreases dataset size

--- Data_processing/retrieve_data.py
+++ Data_processing/retrieve_data.py
@@ -5,9 +5,9 @@ import shutil
 
 from process_data import CategorisedData
 
-data_home = 'F:\Dataset\small_dataset\\'
-image_home = 'F:\Dataset\\train\image\\'
-json_home = 'F:\Dataset\\train\\annos\\'
+new_data_home = '/home/simon/smaller_dataset/'
+image_home = '/home/simon/Downloads/small_dataset/resized_images/'
+json_home = '/home/simon/Downloads/small_dataset/json/'
 
 
 def retrieve_photos():
@@ -21,26 +21,26 @@ def add_json_data():
 
 
 def copy_jsons(categories: List[CategorisedData]):
+    json_dir = new_data_home + 'json'
+    os.makedirs(json_dir, exist_ok=True)
     for category in categories:
         file_names = list(map(lambda file_name: add_file_ending(file_name, file_ending='.json'), category['file_names']))
-        json_dir = data_home + 'json'
-        os.makedirs(json_dir, exist_ok=True)
         for file_name in file_names:
             copy_file(file_name, json_dir, json_home)
 
 
 def read_categories():
-    with open(data_home + 'data_summary.json', 'r') as file:
+    with open(new_data_home + 'data_summary.json', 'r') as file:
         categories: List[CategorisedData] = json.load(file)
         return categories
 
 
 def copy_photos(categories: List[CategorisedData]):
     i = 1
+    image_dir = new_data_home + 'images'
+    os.makedirs(image_dir, exist_ok=True)
     for category in categories:
         file_names = list(map(add_file_ending, category['file_names']))
-        image_dir = data_home + 'category_' + str(i)
-        os.makedirs(image_dir, exist_ok=True)
         for file_name in file_names:
             copy_file(file_name, image_dir, image_home)
         i += 1
@@ -48,9 +48,9 @@ def copy_photos(categories: List[CategorisedData]):
 
 def copy_file(file_name, dest_dir, home):
     src_path = home + file_name
-    dest_path = dest_dir + '\\'
+    dest_path = dest_dir + '/'
     shutil.copy(src_path, dest_path)
 
 
-def add_file_ending(file_name: str, file_ending='.jpg'):
+def add_file_ending(file_name: str, file_ending='_mask.png'):
     return file_name + file_ending
