a927f2f4a7c4ba7e481e15876b37bf5499012068
Ludvig Bennbom <117296547+filipacchi@users.noreply.github.com>
2023-12-07 14:43:44 +0100

    Create masks, onehots etc

--- Data_processing/create_mask.py
+++ Data_processing/create_mask.py
@@ -6,21 +6,21 @@ from PIL import Image
 
 directory = "/Users/ludvigbennbom/Desktop/train/"
 
-category_colors = {
+colors_rgb = {
     1: (255, 0, 0),    # Red
     2: (0, 255, 0),    # Green
-    3: (0, 0, 255),    # Blue
     4: (255, 255, 0),  # Yellow
     5: (255, 0, 255),  # Magenta
-    6: (0, 128, 0),  # Cyan
     7: (128, 0, 0),    # Maroon
     8: (0, 255, 255),    # Green (Dark)
     9: (0, 0, 128),    # Navy
     10: (128, 128, 0),  # Olive
-    11: (128, 0, 128),  # Purple
-    12: (0, 128, 128),  # Teal
-    13: (128, 128, 128) # Gray
+    11: (0, 0, 255), #BLUE
+    12: (0, 128, 0),    #CYANA
 }
+
+colors_bgr = {key: (color[2], color[1], color[0]) for key, color in colors_rgb.items()}
+
 def get_file_paths():
     file_paths = os.listdir("/Users/ludvigbennbom/Desktop/small_dataset/json")
     return file_paths
@@ -29,25 +29,37 @@ def create_mask():
     json_file_paths = get_file_paths()
 
     for file_name in json_file_paths:
-        data = json.load(open("/Users/ludvigbennbom/Desktop/small_dataset/json/" + file_name))
-        img_path = file_name.split(".")[0] + ".jpg"
-        img = cv2.imread("/Users/ludvigbennbom/Desktop/train/image/" + img_path)
-        mask = np.zeros_like(img, dtype=np.uint8)
-        for key in data:
-            if key.startswith("item"):
-                segmentation_list = data[key]["segmentation"]
-                category_id = data[key]["category_id"]
-                color = category_colors.get(category_id, (0, 0, 0))
-                ##points = np.array(segmentation_list)
-                for polygon_coords in segmentation_list:
-                    polygon_coords = np.array(polygon_coords, dtype=np.float32).reshape((-1, 2))
-                    polygon_coords = polygon_coords.astype(np.int32)  
+        if not file_name.startswith("."):
+            data = json.load(open("/Users/ludvigbennbom/Desktop/small_dataset/json/" + file_name))
+            img_path = file_name.split(".")[0] + ".jpg"
+            img = cv2.imread("/Users/ludvigbennbom/Desktop/train/image/" + img_path)
+            mask = np.zeros_like(img, dtype=np.uint8)
+            for key in data:
+                if key.startswith("item"):
+                    segmentation_list = data[key]["segmentation"]
+                    category_id = data[key]["category_id"]
+                    color = colors_bgr.get(category_id, (0, 0, 0))
+                    for polygon_coords in segmentation_list:
+                        polygon_coords = np.array(polygon_coords, dtype=np.float32).reshape((-1, 2))
+                        polygon_coords = polygon_coords.astype(np.int32)
 
-                    cv2.fillPoly(mask, [polygon_coords], color)  
+                        cv2.fillPoly(mask, [polygon_coords], color)
 
-                # Displaying the image
-        mask_image = Image.fromarray(mask)
-        mask_image.save('/Users/ludvigbennbom/Desktop/small_dataset/masks/' + file_name.split(".")[0] + '_mask.png')  # Save with a unique name
+            resized_mask = cv2.resize(mask, (224, 224), interpolation=cv2.INTER_NEAREST)
+            resized_image = cv2.resize(img, (224, 224), interpolation=cv2.INTER_NEAREST)
+            cv2.imwrite('/Users/ludvigbennbom/Desktop/dataset_inter_nearest/masks_30k/' + file_name.split(".")[0] + '_mask.png', resized_mask)
+            cv2.imwrite('/Users/ludvigbennbom/Desktop/dataset_inter_nearest/images_30k/' + file_name.split(".")[0] + '_image.png', resized_image)
 
+
+def create_file_paths():
+    data = json.load(open("/Users/ludvigbennbom/Desktop/dataset500/data_summary.json"))
+    file_paths = []
+    for category in data:
+        cat_id = category["cat_id"]
+        file_names = category["file_names"]
+
+        # Loop through each file in the current category
+        for file_name in file_names:
+            file_paths.append(file_name)
 
 create_mask()
--- Data_processing/onehot_encoded_masks.py
+++ Data_processing/onehot_encoded_masks.py
@@ -3,25 +3,26 @@ import cv2
 import numpy as np
 
 
-directory = "/Users/ludvigbennbom/Desktop/small_dataset_resized/masks_resized"
+directory = "/Users/ludvigbennbom/Desktop/dataset500/masks"
 width = 224
 height = 224
 layers = 11 #10 categories and background
 
 color_dict = {
     0: (255, 0, 0),    # Red
-    1: (0, 0, 255),    # Blue
-    2: (255, 255, 0),  # Yellow
-    3: (0, 128, 0),  # Dark Green
-    4: (128, 0, 0),    # Maroon
-    5: (0, 255, 255),    # Cyan
-    6: (0, 0, 128),    # Navy
-    7: (128, 128, 0),  # Olive
-    8: (128, 0, 128),  # Purple
-    9: (128, 128, 128), # Gray
+    1: (0, 255, 0),    # Green
+    2: (0, 0, 255),    # Blue
+    3: (255, 255, 0),  # Yellow
+    4: (255, 0, 255),  # Magenta
+    5: (0, 128, 0),  # CYAN
+    6: (128, 0, 0),    # Maroon
+    7: (0, 255, 255),    # Green (Dark)
+    8: (0, 0, 128),    # Navy
+    9: (128, 128, 0),  # Olive
     10: (0,0,0)         # Black
-
 }
+
+
 def find_matching_category(color_to_check):
     for category, color in color_dict.items():
         if color_to_check == color:
@@ -35,15 +36,18 @@ def get_file_paths():
 def create_onehot_masks():
     file_paths = get_file_paths()
     for file in file_paths:
-        image = cv2.imread(directory+"/"+file)
-        mask = np.zeros((width, height, layers), dtype=np.uint8)
+        if not file.startswith('.'):
 
-        for x in range(width):
-            for y in range(height):
+            image = cv2.imread(directory+"/"+file)
+            mask = np.zeros((width, height, layers), dtype=np.uint8)
 
-                b, g, r = image[y, x]
-                matching_category = find_matching_category((r,g,b))
-                if matching_category is not None:
-                    mask[y,x,matching_category] = 1
-        np.save('/Users/ludvigbennbom/Desktop/small_dataset_resized/one_hot_encoded_masks/' + file.split(".")[0] + '.npy', mask)
+            for x in range(width):
+                for y in range(height):
+
+                    b, g, r = image[y, x]
+                    matching_category = find_matching_category((r, g, b))
+
+                    if matching_category is not None:
+                        mask[y,x,matching_category] = 1
+            np.save('/Users/ludvigbennbom/Desktop/dataset500/onehotstest/' + file.split(".")[0] + '.npy', mask)
 create_onehot_masks()
